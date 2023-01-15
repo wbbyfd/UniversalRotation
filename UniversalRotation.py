14567537ccb9d9f7f803f33038a77cf744d9a6ef
wbbyfd <wbbyfd@163.com>
2023-01-15 18:27:31 +0800

    对场内基金进行Favorite标记，方便关注自己心仪的基金

--- UniversalRotation.py
+++ UniversalRotation.py
@@ -133,15 +133,15 @@ def rotate_fund_by_premium_rate_and_20net_asset_value(source_sheets: str, dest_r
 
     sheet_fund = wb.sheets[source_sheets]
     # 数据表范围请参考Excel里的"最新数据"区域
-    source_range = 'H2:W' + str(sheet_fund.used_range.last_cell.row)
+    source_range = 'H2:X' + str(sheet_fund.used_range.last_cell.row)
     print('数据表范围：' + source_range)
     data_fund = pandas.DataFrame(sheet_fund.range(source_range).value,
                                        columns=['基金代码','基金名称','投资种类','溢价率','5天净值增长率','10天净值增长率',
                                                 '20天净值增长率','60天净值增长率','120天净值增长率','250天净值增长率',
                                                 '500天净值增长率','750天净值增长率','总排名分','20天净值增长率排名分',
-                                                '溢价率排名分','成交额(万元)'])
+                                                '溢价率排名分','成交额(万元)','Mark'])
     refresh_time = str(time.strftime("%Y-%m-%d__%H-%M-%S", time.localtime()))
-    sheet_fund.range('Y6').value = '更新时间:' + refresh_time
+    sheet_fund.range('Z6').value = '更新时间:' + refresh_time
     log_file = open('log-' + source_sheets + refresh_time + '.txt', 'a+')
 
     for i,fund_code in enumerate(data_fund['基金代码']):
